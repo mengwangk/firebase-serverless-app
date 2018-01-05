a99758a2845bd1759fed52a4fffbf4a072e6ce27
Koh Meng Wang <mengwang@starsystems.local>
2018-01-05 19:03:28 +0800

    Updated files

--- server/functions/index.js
+++ server/functions/index.js
@@ -8,4 +8,10 @@ app.get('/timestamp', (request, response) => {
     response.send(`${Date.now()}`);
 });
 
+app.get('/timestamp-cached', (request, response) => {
+    response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+    response.send(`${Date.now()}`);
+});
+
+
 exports.app = functions.https.onRequest(app);
