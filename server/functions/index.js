a89d3d6b89ecefe97e53676054c3d4e2d9729e15
Koh Meng Wang <mengwangk@gmail.com>
2018-01-06 10:08:03 +0800

    Updated files

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,17 +1,37 @@
 'use strict';
 
 const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
 const express = require('express');
+const engines = require('consolidate');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const firebaseApp = firebase.initializeApp(
+    functions.config().firebase
+);
+
+function getFacts(){
+    const ref = firebaseApp.database().ref('facts');
+    return ref.once('value').then(snap => snap.val());
+}
 
 const app = express();
-app.get('/timestamp', (request, response) => {
-    response.send(`${Date.now()}`);
-});
 
-app.get('/timestamp-cached', (request, response) => {
+// view engine setup
+app.engine('mustache', engines.mustache);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'mustache');
+
+app.get('/', (request, response) => {
     response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-    response.send(`${Date.now()}`);
+    getFacts().then(facts => {
+        response.render('index', {'name': 'hello world'});
+    });
 });
-
 
 exports.app = functions.https.onRequest(app);
